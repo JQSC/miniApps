be8fb6934a41290b3ea5cb28fe5b099fef02be41
高志龙 <gao.zhilong@cgtn.com>
2018-06-22 10:55:49 +0800

    “图片预览

--- sina-comment/pages/component/wb-card/wb-card-component.js
+++ sina-comment/pages/component/wb-card/wb-card-component.js
@@ -15,10 +15,7 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {
-    // 这里是一些组件内部数据
-    someData: {}
-  },
+  data: {},
   attached: function() {
     /**
      * WxParse.wxParse(bindName , type, data, target,imagePadding)
@@ -29,16 +26,40 @@ Component({
      * 5.imagePadding为当图片自适应是左右的单一padding(默认为0,可选)
      */
     var htmlEle = this.data.item;
-    var htmlText = htmlEle ? htmlEle.text : "" ;
+    var htmlText = htmlEle ? htmlEle.text : "";
     WxParse.wxParse('text', 'html', htmlText, this, 2);
+    //整合图片列表
+    var pics = this.setPicsList();
+    this.setData({
+      pics: pics
+    })
   },
   methods: {
+    setPicsList: function() {
+      var item = this.data.item;
+      var pics="";
+      if (item && item.pics) {
+        pics = item.pics.map(function(o, i) {
+          return o.large.url
+        })
+      }
+      return pics
+    },
     // 这里是一个自定义方法
     customMethod: function(e) {
       var item = e.currentTarget.dataset.item;
       getApp().globalData.comments = item;
       wx.navigateTo({
         url: '../wb-api/comment/comment?mid=' + item.mid
+      })
+    },
+    previewImage: function(e) {
+      var current = e.target.dataset.src;
+      
+      console.log(current, this.data.pics)
+      wx.previewImage({
+        current: current, // 当前显示图片的http链接  
+        urls: this.data.pics // 需要预览的图片http链接列表  
       })
     }
   }
